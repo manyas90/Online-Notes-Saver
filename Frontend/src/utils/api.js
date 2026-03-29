e27b75bc37cf7531d5e72e5abd9b3292b8ee5751
Manoj Ahire <manojahire7673@gmail.com>
2026-03-29 19:01:04 +0530

    Update api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,5 +1,6 @@
-export const BASE_URL = "https://online-notes-saver-backend.onrender.com";
+export const BASE_URL = "https://online-notes-saver-backend.onrender.com/api";
+
 export const AUTH_URL = `${BASE_URL}/auth`;
 export const PASTE_URL = `${BASE_URL}/paste`;
+export const NOTES_URL = `${BASE_URL}/notes`;
 export const DELETE_URL = `${BASE_URL}/notes`;
-export const NOTES_URL = "https://online-notes-saver-backend.onrender.com/api";";
